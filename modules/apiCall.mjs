04e0d2b477ef7010c37100b58dfd0741b7d957dd
Henrik Almqvist <almqvist.henric@gmail.com>
2022-01-20 17:14:44 +0100

    Changed a few things

--- modules/apiCall.mjs
+++ modules/apiCall.mjs
@@ -1,25 +1,27 @@
 
-export function exportApi() {
-    
-    let button = document.getElementById('button');
+ export async function sendApiRequest(dateAPI, rover) {
+        
 
-    let dateAPI = "2021-05-10";
-    let rover = "Curiosity";
+        let dateYear = "";
 
-    button.addEventListener("click", () => {
-        sendApiRequest(dateAPI, rover);
-    });
+        if (rover === "Curiosity") {
+            dateYear = "2021-";
+        } else if (rover === "Spirit") {
+            dateYear = "2005-";
+        } else if (rover === "Opportunity") {
+            dateYear = "2007-";
+        }
 
+        let apiDate = dateYear + dateAPI;
 
-   
-    async function sendApiRequest(dateAPI, rover) {
-        debugger;
         let API_KEY = "XZ8Ryto558Nax2OqbPAJsYsSlx7J6qTqPOCcWusS"
-        let response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${dateAPI}&api_key=${API_KEY}`);
-        //console.log(response);
-        let data = await response.json()
+        let response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${apiDate}&page=1&api_key=${API_KEY}`);
+        let data = await response.json();
         console.log(data);
-
+        debugger;
+        return data;
     }
-}
+
+    
+
 
--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-import { exportApi } from './modules/apiCall.mjs';
+import { sendApiRequest } from './modules/apiCall.mjs';
+import { exportShowElement } from './modules/showElement.mjs';
 
 
-let test = exportApi();
+exportShowElement();
+
+
+let button = document.getElementById('button');
+
+let inputDate = "05-10";
+let inputRover = "Curiosity";
+
+
+
+let test =  button.addEventListener("click", () => {
+     await sendApiRequest(inputDate, inputRover);
+});
+
 
 console.log(test);
+
